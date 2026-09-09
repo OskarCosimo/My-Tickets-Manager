36f5fb45bb0b52de6301bfbc97371525450a6ccf
Oscar Cosimo <OskarCosimo@users.noreply.github.com>
2026-09-09 20:02:28 +0200

    Update application version to 1.0.15

--- includes/version.php
+++ includes/version.php
@@ -2,5 +2,5 @@
 // includes/version.php
 // Current installed application version and GitHub repository details
 
-define('APP_VERSION', '1.0.14');
+define('APP_VERSION', '1.0.15');
 define('GITHUB_REPO', 'OskarCosimo/My-Tickets-Manager');
